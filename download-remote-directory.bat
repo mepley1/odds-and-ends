ea085540317af3096fe1b8f2b86f1f7b60009fa9
Mike E <47926856+alienemoji@users.noreply.github.com>
2021-04-24 14:41:14 -0500

    Use ssh keys to dl remote dir

--- download-remote-directory.bat
+++ download-remote-directory.bat
@@ -1,1 +1,3 @@
+:: Use SSH keys to download remote linux dir from Windows client
+:: Local target directory must exist
 scp -r -i "C:\Users\username\.ssh\id_rsa" user@host:"/home/pathto/remotedir" "E:\pathto\localdir"
