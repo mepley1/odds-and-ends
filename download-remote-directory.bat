c2ffdadd8b7cd52a767b820adfd2b1a1d5862171
Mike E <47926856+alienemoji@users.noreply.github.com>
2021-11-13 17:46:00 -0600

    Update download-remote-directory.bat

--- download-remote-directory.bat
+++ download-remote-directory.bat
@@ -1,3 +1,3 @@
-:: Use SSH keys to download remote linux dir from Windows client
+:: Use SCP w/ SSH keys to download remote linux dir from Windows client
 :: Local target directory must exist
 scp -r -i "C:\Users\username\.ssh\id_rsa" user@host:"/home/pathto/remotedir" "E:\pathto\localdir"
