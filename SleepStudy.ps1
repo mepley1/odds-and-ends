20f255db601aee05f0a5922cddba380681506b35
Mike E <47926856+mepley1@users.noreply.github.com>
2023-01-11 23:35:51 -0600

    Generate SleepStudy report

---
+++ SleepStudy.ps1
@@ -0,0 +1,4 @@
+# Generate SleepStudy report for the past 7 days
+# See: https://learn.microsoft.com/en-us/windows-hardware/design/device-experiences/modern-standby-sleepstudy
+
+powercfg /sleepstudy /duration 7
