e9148e0d895207a01118b35a26f02dc4d670c784
Mike E <47926856+mepley1@users.noreply.github.com>
2023-01-15 14:37:48 -0600

    Check whether a list contains duplicate elements

--- contains-dupes.py
+++ contains-dupes.py
@@ -1,9 +1,10 @@
-# Check if a list contains duplicate elements
+# Check whether a list contains duplicate elements
 
 def has_duplicates(lst):
     return len(lst) != len(set(lst))
 
 x = [1,2,3,4,5,5]
 y = [1,2,3,4,5]
-has_duplicates(x) # True
-has_duplicates(y) # False
+
+print(has_duplicates(x)) # True
+print(has_duplicates(y)) # False
